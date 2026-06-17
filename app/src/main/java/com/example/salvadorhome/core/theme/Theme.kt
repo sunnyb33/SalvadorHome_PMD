96e3943c504ad4e9b7aa847cf1a13c86b0086d12
Suni <00010124@uca.edu.sv>
2026-06-17 13:42:10 -0600

    :sparkles: Se realizan las pantallas principales

--- app/src/main/java/com/example/salvadorhome/core/theme/Theme.kt
+++ app/src/main/java/com/example/salvadorhome/core/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.example.salvadorhome.ui.theme
+package com.example.salvadorhome.core.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
--- app/src/main/java/com/example/salvadorhome/core/theme/Type.kt
+++ app/src/main/java/com/example/salvadorhome/core/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.salvadorhome.ui.theme
+package com.example.salvadorhome.core.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
